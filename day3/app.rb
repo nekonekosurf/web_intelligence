2fe3abbada178f24294f1daa6e8e52643bce4be6
nekonekosurf <nekonekosurf88@gmail.com>
2019-12-02 19:23:30 +0900

    university PC

--- day3/app.rb
+++ day3/app.rb
@@ -2,7 +2,8 @@
 # coding: utf-8
 require 'sinatra'
 require './yolp'
-
+require "open-uri"
+require "json"
 get '/' do
   erb :putInformation
 end
@@ -11,14 +12,18 @@ end
 get '/result' do
   yolp = YOLP.new
 	coord = yolp.coordinate("#{params[:address]}")
+	if "#{params[:address]}"=="" or coord==nil
+		redirect '/'
+	end
+
 	@basic_adress = "#{params[:address]}"
 	@ido = "#{coord[1]}"
 	@keido ="#{coord[0]}"
-  @dist="#{yolp.distance([coord[1],coord[0]],[43.5,135.5])}"
+
 
   # @library_name=get_library_name("BB21074902")
   # hash=get_library_name("AA12007953") 		#ここで図書館の名前と住所をハッシュに入れている
-  hash=get_library_name("BB21074902") 		#ここで図書館の名前と住所をハッシュに入れている
+  hash=get_library_name("#{params[:book_id]}") 		#ここで図書館の名前と住所をハッシュに入れている
 																							#図書館名=>住所
 																							#{"公立はこだて未来大学 情報ライブラリー"=>"函館市亀田中野町116-2", "三重大学 附属図書館"=>"三重県津市栗真町屋町 1577", "大阪電気通信大学 図書館"=>"大阪府寝屋川市初町18-8"}
 
@@ -27,6 +32,11 @@ get '/result' do
   hash2=Hash.new { |h,k| h[k] = {} }
     hash.each{|name,address|
      target_library = yolp.coordinate(address)
+if target_library==nil
+	puts "nillllllllll!"
+	next
+end
+
     	dist_=yolp.distance([coord[1],coord[0]],[target_library[1],target_library[0]])
 		#	puts "#{name} と#{address} との距離 #{yolp.distance([coord[1],coord[0]],[target_library[1],target_library[0]])}"
 			  hash2[hash[name]]=dist_
@@ -43,26 +53,22 @@ get '/result' do
 		puts
     puts array_order
  		@array_order = array_order
-
-
-
  	 erb :display
 end
 
-
-
-
-require "open-uri"
-require "json"
 def get_adrress(id)
   baseidURL = id
   target= baseidURL.gsub(/^http:/,"https:")
   target= target+".json"
+begin 
   open(target){|f|
     hash = JSON.load(f)
     p hash["@graph"][0]["v:adr"]["v:label"]
     return hash["@graph"][0]["v:adr"]["v:label"]
   }
+rescue => e
+puts e
+end
 end
 
 def get_library_name(key_w)
@@ -70,6 +76,7 @@ def get_library_name(key_w)
   target= baseidURL.gsub(/^http:/,"https:")
   file = File.open('result.json', "w");
   reHash={}
+begin
   open(target){|f|
     hash = JSON.load(f)
     i = 1
@@ -84,5 +91,9 @@ def get_library_name(key_w)
       i +=1
     end
   }
+rescue=>e
+puts e
+redirect '/'
+end
   return reHash
 end
