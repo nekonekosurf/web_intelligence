38f5c5dbffc51aaf65ba63780590305e21307602
nekonekosurf <nekonekosurf88@gmail.com>
2019-12-03 11:38:41 +0900

    ほぼ終了

--- day3/app.rb
+++ day3/app.rb
@@ -1,99 +1,95 @@
 #!/usr/bin/env ruby
-# coding: utf-8
+# frozen_string_literal: true
+
 require 'sinatra'
 require './yolp'
-require "open-uri"
-require "json"
+require 'open-uri'
+require 'json'
 get '/' do
   erb :putInformation
 end
 
-
 get '/result' do
   yolp = YOLP.new
-	coord = yolp.coordinate("#{params[:address]}")
-	if "#{params[:address]}"=="" or coord==nil
-		redirect '/'
-	end
+  coord = yolp.coordinate(params[:address].to_s)
+  redirect '/' if (params[:address].to_s == '') || coord.nil?
 
-	@basic_adress = "#{params[:address]}"
-	@ido = "#{coord[1]}"
-	@keido ="#{coord[0]}"
-
+  @basic_adress = params[:address].to_s
+  @ido = (coord[1]).to_s
+  @keido = (coord[0]).to_s
 
   # @library_name=get_library_name("BB21074902")
-  # hash=get_library_name("AA12007953") 		#ここで図書館の名前と住所をハッシュに入れている
-  hash=get_library_name("#{params[:book_id]}") 		#ここで図書館の名前と住所をハッシュに入れている
-																							#図書館名=>住所
-																							#{"公立はこだて未来大学 情報ライブラリー"=>"函館市亀田中野町116-2", "三重大学 附属図書館"=>"三重県津市栗真町屋町 1577", "大阪電気通信大学 図書館"=>"大阪府寝屋川市初町18-8"}
-
+  # hash=get_library_name("AA12007953")     #ここで図書館の名前と住所をハッシュに入れている
+  hash = get_library_name(params[:book_id].to_s) # ここで図書館の名前と住所をハッシュに入れている
+  # 図書館名=>住所
+  # {"公立はこだて未来大学 情報ライブラリー"=>"函館市亀田中野町116-2", "三重大学 附属図書館"=>"三重県津市栗真町屋町 1577", "大阪電気通信大学 図書館"=>"大阪府寝屋川市初町18-8"}
 
   @hash = hash
-  hash2=Hash.new { |h,k| h[k] = {} }
-    hash.each{|name,address|
-     target_library = yolp.coordinate(address)
-if target_library==nil
-	puts "nillllllllll!"
-	next
-end
+  hash2 = Hash.new { |h, k| h[k] = {} }
+  hash.each do |name, address|
+    target_library = yolp.coordinate(address)
+    if target_library.nil?
+      puts 'nillllllllll!'
+      next
+    end
 
-    	dist_=yolp.distance([coord[1],coord[0]],[target_library[1],target_library[0]])
-		#	puts "#{name} と#{address} との距離 #{yolp.distance([coord[1],coord[0]],[target_library[1],target_library[0]])}"
-			  hash2[hash[name]]=dist_
-		}
-		temp= []
-		temp=hash2.sort {|(k1, v1), (k2, v2)| v1 <=> v2 }#
-		p temp[0]
-		array_order=[]
-		for n in temp do
-			p n[0]
-			p hash.invert[n[0]]
-			array_order.push(hash.invert[n[0]])
-		end
-		puts
-    puts array_order
- 		@array_order = array_order
- 	 erb :display
+    dist_ = yolp.distance([coord[1], coord[0]], [target_library[1], target_library[0]])
+    #  puts "#{name} と#{address} との距離 #{yolp.distance([coord[1],coord[0]],[target_library[1],target_library[0]])}"
+    hash2[hash[name]] = dist_
+  end
+  temp = []
+  temp = hash2.sort { |(_k1, v1), (_k2, v2)| v1 <=> v2 }
+  p temp[0]
+  array_order = []
+  temp.each do |n|
+    p n[0]
+    p hash.invert[n[0]]
+    array_order.push(hash.invert[n[0]])
+  end
+  puts
+  puts array_order
+  @array_order = array_order
+  erb :display
 end
 
 def get_adrress(id)
   baseidURL = id
-  target= baseidURL.gsub(/^http:/,"https:")
-  target= target+".json"
-begin 
-  open(target){|f|
-    hash = JSON.load(f)
-    p hash["@graph"][0]["v:adr"]["v:label"]
-    return hash["@graph"][0]["v:adr"]["v:label"]
-  }
-rescue => e
-puts e
-end
+  target = baseidURL.gsub(/^http:/, 'https:')
+  target += '.json'
+  begin
+    open(target) do |f|
+      hash = JSON.load(f)
+      p hash['@graph'][0]['v:adr']['v:label']
+      return hash['@graph'][0]['v:adr']['v:label']
+    end
+  rescue StandardError => e
+    puts e
+  end
 end
 
 def get_library_name(key_w)
-  baseidURL = "https://ci.nii.ac.jp/ncid/"+key_w+".json"
-  target= baseidURL.gsub(/^http:/,"https:")
-  file = File.open('result.json', "w");
-  reHash={}
-begin
-  open(target){|f|
-    hash = JSON.load(f)
-    i = 1
-    for n in hash["@graph"][0]["bibo:owner"] do
-      #puts i
-      #puts n["foaf:name"]
-      # reHash[n["foaf:name"]]= get_adrress(n["@id"])
-      reHash.store("#{n["foaf:name"]}", get_adrress(n["@id"]))
-    #  puts reHash
-      # get_adrress(n["@id"])
-      puts
-      i +=1
+  baseidURL = 'https://ci.nii.ac.jp/ncid/' + key_w + '.json'
+  target = baseidURL.gsub(/^http:/, 'https:')
+  file = File.open('result.json', 'w')
+  reHash = {}
+  begin
+    open(target) do |f|
+      hash = JSON.load(f)
+      i = 1
+      hash['@graph'][0]['bibo:owner'].each do |n|
+        # puts i
+        # puts n["foaf:name"]
+        # reHash[n["foaf:name"]]= get_adrress(n["@id"])
+        reHash.store((n['foaf:name']).to_s, get_adrress(n['@id']))
+        #  puts reHash
+        # get_adrress(n["@id"])
+        puts
+        i += 1
+      end
     end
-  }
-rescue=>e
-puts e
-redirect '/'
-end
-  return reHash
+  rescue StandardError => e
+    puts e
+    redirect '/'
+  end
+  reHash
 end
--- day4_dev/app.rb
+++ day4_dev/app.rb
@@ -1,147 +1,201 @@
 #!/usr/bin/env ruby
-# coding: utf-8
+# frozen_string_literal: true
+
 require 'sinatra'
 require './yolp'
-require "open-uri"
-require "json"
+require 'open-uri'
+require 'json'
 require 'active_record'
 require 'uri'
 enable :sessions
 
 ActiveRecord::Base.establish_connection(
-  adapter:'sqlite3',
-  database:'sandbox.db'
+  adapter: 'sqlite3',
+  database: 'sandbox.db'
 )
 
-class Library_record< ActiveRecord::Base
+class Library_record < ActiveRecord::Base
 end
 
+hash_count={} #図書名のw出現回数調べる
 
 get '/' do
   erb :putInformation
 end
 
 get '/result_' do
-  # yomikomi=params[:read_val]                                      #とってくる検索結果の数
-  yomikomi=20                                      #とってくる検索結果の数
-  appid ="jsqAbSa3aKX49y0tRjEY"                     #アプリケーションキー
-  key_w="#{params[:key_word]}"                      #検索語
-  @key_word = "#{params[:key_word]}"
-  session[:kijyun] = "#{params[:address]}"          #sessionで基準点を使いまわし
-  baseidURL = "https://ci.nii.ac.jp/books/opensearch/" #キーワードで検索時のURL
-  target= baseidURL.gsub(/^http:/,"https:")
-  key_w = key_w.encode()
-  target = baseidURL+"search?title="+URI.encode(key_w)+"&format=json&appid="+appid
+  yolp = YOLP.new
+  if yolp.coordinate(params[:address].to_s).nil? || (params[:address].to_s == '')
+    redirect '/'
+  end
+  appid = 'jsqAbSa3aKX49y0tRjEY' # アプリケーションキー
+  key_w = params[:key_word].to_s # 検索語
+  @key_word = params[:key_word].to_s
+  session[:kijyun] = params[:address].to_s # sessionで基準点を使いまわし
+  baseidURL = 'https://ci.nii.ac.jp/books/opensearch/' # キーワードで検索時のURL
+  target = baseidURL.gsub(/^http:/, 'https:')
+  key_w = key_w.encode
+  target = baseidURL + 'search?title=' + URI.encode(key_w) + '&format=json&count=200&appid=' + appid
 
-  puts "target"
+  puts 'target'
   puts target
-  hash_title={}                                    #タイトルと住所の組み合わせのハッシュ
-  open(target){|f|
-      hash = JSON.load(f)
-      for n in 1..yomikomi do
-        hash_title.store(hash["@graph"][0]["items"][n-1]["title"],hash["@graph"][0]["items"][n-1]["@id"])
+  hash_title = {} # タイトルと住所の組み合わせのハッシュ
+  open(target) do |f|
+    hash = JSON.load(f)
+    min = hash['@graph'][0]['opensearch:totalResults'].to_i
+    if  hash['@graph'][0]['opensearch:totalResults'] == '0'
+      puts '存在しません'
+      redirect '/'
+    else
+      puts min
+      if min >= 200
+        (1..200).each do |n|
+          if hash['@graph'][0]['items'][n - 1]['title']==nil
+            next
+          else
+            if  hash_title.key?(hash['@graph'][0]['items'][n - 1]['title'])
+              cnt = hash_count[hash['@graph'][0]['items'][n - 1]['title']]
+              cnt +=1
+              hash_title.store(hash['@graph'][0]['items'][n - 1]['title']+ "(" + cnt.to_s + ")", hash['@graph'][0]['items'][n - 1]['@id'])
+            else
+              hash_title.store(hash['@graph'][0]['items'][n - 1]['title'], hash['@graph'][0]['items'][n - 1]['@id'])
+              hash_count[hash['@graph'][0]['items'][n - 1]['title']]=1
+              puts n
+              puts hash['@graph'][0]['items'][n - 1]['title']
+             end
+           end
+          end
+        else
+          (1..min).each do |n|
+            if hash['@graph'][0]['items'][n - 1]['title']==nil
+              next
+            else
+              if  hash_title.key?(hash['@graph'][0]['items'][n - 1]['title'])
+                cnt = hash_count[hash['@graph'][0]['items'][n - 1]['title']]
+                cnt +=1
+                hash_title.store(hash['@graph'][0]['items'][n - 1]['title']+ "(" + cnt.to_s + ")", hash['@graph'][0]['items'][n - 1]['@id'])
+              else
+                hash_title.store(hash['@graph'][0]['items'][n - 1]['title'], hash['@graph'][0]['items'][n - 1]['@id'])
+                hash_count[hash['@graph'][0]['items'][n - 1]['title']]=1
+                puts n
+                puts hash['@graph'][0]['items'][n - 1]['title']
+              end
+            end
+          end
+        end
       end
-  }
+  end
   @hash_title = hash_title
   erb :result_search
 end
 
 get '/result' do
-  yolp = YOLP.new                  #地図を表示させるときに使うようになる
-	coord = yolp.coordinate("#{session[:kijyun]}")       #基準点の経度井戸を計算
-	@basic_adress = "#{session[:kijyun]}"
-	@ido = "#{coord[1]}"
-	@keido ="#{coord[0]}"
-  book_id = "#{params[:value]}".delete!('http://ci.nii.ac.jp/ncid/') #valueには本のID以外にも余計なものが入っているので削除
+  yolp = YOLP.new # 地図を表示させるときに使うようになる
+  coord = yolp.coordinate(session[:kijyun].to_s) # 基準点の経度井戸を計算
+  @basic_adress = session[:kijyun].to_s
+  @ido = (coord[1]).to_s
+  @keido = (coord[0]).to_s
+  book_id = params[:value].to_s.delete!('http://ci.nii.ac.jp/ncid/') # valueには本のID以外にも余計なものが入っているので削除
   session[:book_id] = book_id
-  hash=get_library_name(book_id)  #本のIDで図書館の名前と図書館の住所が入る
-  @hash = hash
-  array_order = libNmaeAdr_to_arrayOrder(hash,"#{session[:kijyun]}")
-  @array_order = array_order
- 	 erb :display
+  hash = get_library_name(book_id) # 本のIDで図書館の名前と図書館の住所が入る
+  if hash.nil?
+    erb :noExist
+  else
+    @hash = hash
+    array_order = libNmaeAdr_to_arrayOrder(hash, session[:kijyun].to_s)
+    @array_order = array_order
+    erb :display
+end
 end
 
-
-#距離のアップデート出きるに出きる毎回get書くのは適切でない気がする
+# 距離のアップデート出きるに出きる毎回get書くのは適切でない気がする
 get '/update_location' do          # 基準住所をアップデート
-  yolp = YOLP.new                  #地図を表示させるときに使うようになる
-	coord = yolp.coordinate("#{params[:address_new]}")       #基準点の経度井戸を計算
-  @basic_adress = "#{params[:address_new]}"
-  @ido = "#{coord[1]}"
-  @keido ="#{coord[0]}"
-  book_id = "#{session[:book_id]}" #valueには本のID以外にも余計なものが入っているので削除
-  hash=get_library_name(book_id)  #本のIDで図書館の名前と図書館の住所が入る
+  yolp = YOLP.new                  # 地図を表示させるときに使うようになる
+  coord = yolp.coordinate(params[:address_new].to_s) # 基準点の経度井戸を計算
+  @basic_adress = params[:address_new].to_s
+  @ido = (coord[1]).to_s
+  @keido = (coord[0]).to_s
+  book_id = session[:book_id].to_s # valueには本のID以外にも余計なものが入っているので削除
+  hash = get_library_name(book_id) # 本のIDで図書館の名前と図書館の住所が入る
   @hash = hash
-  array_order =libNmaeAdr_to_arrayOrder(hash,"#{params[:address_new]}")
+  array_order = libNmaeAdr_to_arrayOrder(hash, params[:address_new].to_s)
   @array_order = array_order
   erb :display
-
 end
 
-
-def libNmaeAdr_to_arrayOrder(hash,kijyun)  #図書館の名前と住所ハッシュ基準値から距離順並べ配返す
-  yolp = YOLP.new                  #地図を表示させるときに使うようになる
-	coord = yolp.coordinate("#{session[:kijyun]}")       #基準点の経度井戸を計算
-  hash2=Hash.new { |h,k| h[k] = {} }#hashのの情報をすべて距離計算する
-  hash.each{|name,address|
-     # target_library = yolp.coordinate(address)
-     lib = Library_record.find_by(library_name:name)
-     dist_=yolp.distance([coord[1],coord[0]],[lib.ido,lib.keido])
-     hash2[hash[name]]=dist_  #hashとhash2の二重ハッシュ｛図書館の名前＝＞｛図書館の住所＝＞基準との距離｝｝
-  }
-  temp= []
-  temp=hash2.sort {|(k1, v1), (k2, v2)| v1 <=> v2 }#距離順でソートしたのを配列に
-  array_order=[]
-  for n in temp do
+def libNmaeAdr_to_arrayOrder(hash, _kijyun) # 図書館の名前と住所ハッシュ基準値から距離順並べ配返す
+  yolp = YOLP.new # 地図を表示させるときに使うようになる
+  coord = yolp.coordinate(session[:kijyun].to_s) # 基準点の経度井戸を計算
+  hash2 = Hash.new { |h, k| h[k] = {} } # hashのの情報をすべて距離計算する
+  hash.each do |name, _address|
+    # target_library = yolp.coordinate(address)
+    lib = Library_record.find_by(library_name: name)
+    dist_ = yolp.distance([coord[1], coord[0]], [lib.ido, lib.keido])
+    hash2[hash[name]] = dist_ # hashとhash2の二重ハッシュ｛図書館の名前＝＞｛図書館の住所＝＞基準との距離｝｝
+  end
+  temp = []
+  temp = hash2.sort { |(_k1, v1), (_k2, v2)| v1 <=> v2 } # 距離順でソートしたのを配列に
+  array_order = []
+  temp.each do |n|
     # p n[0]
     # p hash.invert[n[0]]
-    array_order.push(hash.invert[n[0]])  #ソートした距離から図書館の名前を取り出している
+    array_order.push(hash.invert[n[0]]) # ソートした距離から図書館の名前を取り出している
   end
 
-  return array_order
+  array_order
 end
 
 def get_adrress(id)
   baseidURL = id
-  target= baseidURL.gsub(/^http:/,"https:")
-  target= target+".json"
-  open(target){|f|
+  target = baseidURL.gsub(/^http:/, 'https:')
+  target += '.json'
+  open(target) do |f|
     hash = JSON.load(f)
-    p hash["@graph"][0]["v:adr"]["v:label"]
-    return hash["@graph"][0]["v:adr"]["v:label"]
-  }
+    p hash['@graph'][0]['v:adr']['v:label']
+    return hash['@graph'][0]['v:adr']['v:label']
+  end
 end
 
-def get_library_name(key_w)               #本のIDが引数として与えられる
-  baseidURL = "https://ci.nii.ac.jp/ncid/"+key_w+".json"
-  target= baseidURL.gsub(/^http:/,"https:")
-  reHash={}
-  open(target){|f|
+def get_library_name(key_w) # 本のIDが引数として与えられる
+  baseidURL = 'https://ci.nii.ac.jp/ncid/' + key_w + '.json'
+  target = baseidURL.gsub(/^http:/, 'https:')
+  reHash = {}
+  open(target) do |f|
     hash = JSON.load(f)
-    i = 1
-    for n in hash["@graph"][0]["bibo:owner"] do
-      #データベース用い高速に
-        #もし存在しなければ追加
-        if !Library_record.where(library_name:"#{n["foaf:name"]}").exists?
-          new_address=get_adrress(n["@id"])
-          yolp = YOLP.new                  #地図を表示させるときに使うようになる
-          target_library = yolp.coordinate(new_address) #ここで緯度と経度を計算しておく
-          lib= Library_record.new(
-            library_name:"#{n["foaf:name"]}",
-            library_address:new_address,
-            ido:target_library[1],
-            keido:target_library[0]
+    if hash['@graph'][0]['cinii:ownerCount'] == '0'
+      puts 'noExist'
+      return nil
+    else
+      puts 'Exist!!!'
+      i = 1
+      hash['@graph'][0]['bibo:owner'].each do |n|
+        # データベース用い高速に
+        # もし存在しなければ追加
+        if !Library_record.where(library_name: (n['foaf:name']).to_s).exists?
+          new_address = get_adrress(n['@id'])
+          yolp = YOLP.new # 地図を表示させるときに使うようになる
+          target_library = yolp.coordinate(new_address) # ここで緯度と経度を計算しておく
+          if target_library.nil?
+            puts 'nill!!!!!!!!!!'
+            next
+          end
+          lib = Library_record.new(
+            library_name: (n['foaf:name']).to_s,
+            library_address: new_address,
+            ido: target_library[1],
+            keido: target_library[0]
           )
           lib.save
-          reHash.store("#{n["foaf:name"]}", new_address)  #図書館の名前=>住所でハッシュ
-        #もし存在すればデータベースから読み出し
+          reHash.store((n['foaf:name']).to_s, new_address) # 図書館の名前=>住所でハッシュ
+        # もし存在すればデータベースから読み出し
         else
-          name_address = Library_record.find_by(library_name:"#{n["foaf:name"]}")
-          reHash.store("#{n["foaf:name"]}", name_address.library_address)    #図書館の名前=>住所でハッシュ
+          name_address = Library_record.find_by(library_name: (n['foaf:name']).to_s)
+          reHash.store((n['foaf:name']).to_s, name_address.library_address) # 図書館の名前=>住所でハッシュ
         end
-      i +=1
-    end
-  }
-  return reHash
+        i += 1
+      end
+  end
+  end
+  reHash
 end
 # CREATE TABLE library_records(id integer PRIMARY KEY, library_name text,library_address text,ido REAL,keido REAL);
